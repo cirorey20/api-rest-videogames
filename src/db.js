8bb9293990a2d27e57c4a5d5ffc8c3a6537c4880
cirorey20 <cirorey95@gmail.com>
2022-04-25 16:18:34 -0300

    apply 4

--- src/db.js
+++ src/db.js
@@ -9,47 +9,46 @@ const {
 } = process.env;
 //PRODUCCION
 
-// const { Client } = require('pg');
+const { Client } = require('pg');
 
-// const sequelize = new Client({
-  //   connectionString: DATABASE_URL,
-  //   ssl: {
-  //     rejectUnauthorized: false
-  //   }
-  // });
-// sequelize.connect();
-    
-//PRODUCCION
-
-const sequelize = new Sequelize(DATABASE_URL, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
-
-const basename = path.basename(__filename);
-
-const modelDefiners = [];
-
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+const sequelize = new Client({
+    connectionString: DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
+  
+  //PRODUCCION
+  
+  // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+    //   logging: false, // set to console.log to see the raw SQL queries
+    //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    // });
+    
+    const basename = path.basename(__filename);
+    
+    const modelDefiners = [];
+    
+    // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+    fs.readdirSync(path.join(__dirname, '/models'))
+    .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+    .forEach((file) => {
+      modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    });
+    
+    // Injectamos la conexion (sequelize) a todos los modelos
+    modelDefiners.forEach(model => model(sequelize));
+    // Capitalizamos los nombres de los modelos ie: product => Product
+    let entries = Object.entries(sequelize.models);
+    let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+    sequelize.models = Object.fromEntries(capsEntries);
+    
+    // En sequelize.models están todos los modelos importados como propiedades
+    // Para relacionarlos hacemos un destructuring
+    const { Videogame, Genere, User } = sequelize.models;
 
-// Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
-sequelize.models = Object.fromEntries(capsEntries);
-
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
-const { Videogame, Genere, User } = sequelize.models;
-
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews); N:N
+    // Aca vendrian las relaciones
+    // Product.hasMany(Reviews); N:N
 Videogame.belongsToMany(Genere, { through: "videogame_genere" });
 Genere.belongsToMany(Videogame, { through: "videogame_genere" });
 
@@ -57,6 +56,7 @@ Genere.belongsToMany(Videogame, { through: "videogame_genere" });
 User.hasMany(Videogame, { as: 'videogames', foreignKey: 'userId' } );
 Videogame.belongsTo(User, { as: 'user' })
 
+sequelize.connect();
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
