d4eebbbaf5e791ab946ea7f4868ebda3f5a9241b
cirorey20 <cirorey95@gmail.com>
2022-04-25 17:46:52 -0300

    apply 7

--- src/db.js
+++ src/db.js
@@ -8,10 +8,16 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DATABASE_URL
 } = process.env;
 
-  const sequelize = new Sequelize(DATABASE_URL, {
-      dialect: 'postgres',
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  // const sequelize = new Sequelize(DATABASE_URL, {
+  //     dialect: 'postgres',
+  //     logging: false, // set to console.log to see the raw SQL queries
+  //     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  // });
+  const sequelize = new Sequelize({
+    connectionString: DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
     
     const basename = path.basename(__filename);
