44376e7a68ddc0fc7a9d5ffc17d20e04a1f0ede1
cirorey20 <cirorey95@gmail.com>
2022-04-25 16:16:36 -0300

    apply 3

--- src/db.js
+++ src/db.js
@@ -21,10 +21,11 @@ const {
     
 //PRODUCCION
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
