810fb35d0a9edeb9b3093d3463d9dc0886cadad3
cirorey20 <cirorey95@gmail.com>
2022-04-27 14:42:57 -0300

    aply 5

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -36,6 +36,6 @@ const sequelize = new Sequelize(LINK, {
 setupModels(sequelize);
 
 //si lo pongo en true se reinicia con cada cambio
-// sequelize.sync({ force: false });
+sequelize.sync({ force: false });
 
 module.exports = sequelize;
