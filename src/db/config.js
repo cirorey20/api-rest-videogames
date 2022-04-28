36bcc66149bdaf3e6f3e70c4a75a5b7da745b70c
cirorey20 <cirorey95@gmail.com>
2022-04-28 17:43:32 -0300

    deploy 36

--- src/db/config.js
+++ src/db/config.js
@@ -13,18 +13,19 @@ module.exports = {
   production: {
     // url: config.dbUrl,
     connectionString: config.dbUrl,
-    // use_env_variable: 'DATABASE_URL', //IMPORTANTE si quiero hacer deploy en sequelize con un ORM
-    // dialect: 'postgres',
-    ssl: {      /* <----- Add SSL option */
-        require: true,
-        rejectUnauthorized: false 
-    },
-    // logging: false,
-    //     dialectOptions: {
-    //   ssl: {      /* <----- Add SSL option */
+    use_env_variable: 'DATABASE_URL', //IMPORTANTE si quiero hacer deploy en sequelize con un ORM
+    dialect: 'postgres',
+    protocol: null,
+    // ssl: {      /* <----- Add SSL option */
     //     require: true,
     //     rejectUnauthorized: false 
-    //   }
     // },
+    logging: false,
+        dialectOptions: {
+      ssl: {      /* <----- Add SSL option */
+        require: true,
+        rejectUnauthorized: false 
+      }
+    },
   }
 }
