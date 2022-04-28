d984e0b3882d9447e44d841ab71ce71becd08d9f
cirorey20 <cirorey95@gmail.com>
2022-04-28 00:59:01 -0300

    deploy 7

--- index.js
+++ index.js
@@ -9,30 +9,30 @@ const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:3000', 'https://myapiboyar.com'];
-const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('no permitido'))
-    }
-  }
-}
-app.use(cors(options));
+// const whitelist = ['http://localhost:3000', 'https://myapiboyar.com'];
+// const options = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('no permitido'))
+//     }
+//   }
+// }
+// app.use(cors(options));
 
 app.get('/', (req, res, next) => {
-  res.redirect('/videogames')
-  // res.send("Esta funcionando correctamente!");
+  // res.redirect('/videogames')
+  res.send("Esta funcionando correctamente!");
 })
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
 
 app.use(router)
 require('./src/strategies');
