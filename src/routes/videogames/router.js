20568262f0ad60e576b4f7aa2bc18897ac49c814
cirorey20 <cirorey95@gmail.com>
2022-04-28 21:16:21 -0300

    deploy switch apidb

--- src/routes/videogames/router.js
+++ src/routes/videogames/router.js
@@ -8,29 +8,29 @@ router.get('/', async(req,res, next)=> { //[ ] GET /videogames
 
     try {        
         
-        const game = req.query.name;
-        if(game) {
-            //dbSearch
-            let db = await getDbGames();
-            let games = db.filter((el) => {
-                return el.name.toLowerCase().includes(game.toLowerCase())
-            })
+        // const game = req.query.name;
+        // if(game) {
+        //     //dbSearch
+        //     let db = await getDbGames();
+        //     let games = db.filter((el) => {
+        //         return el.name.toLowerCase().includes(game.toLowerCase())
+        //     })
             
-            //apiSearch
-            let searchApi = await searchApiGames(game);
-            let limitSearch = searchApi.slice(0,15)//limito mi busqueda a primeros 15
+        //     //apiSearch
+        //     let searchApi = await searchApiGames(game);
+        //     let limitSearch = searchApi.slice(0,15)//limito mi busqueda a primeros 15
             
-            let joinSearch = games.concat(limitSearch);
+        //     let joinSearch = games.concat(limitSearch);
             
-            if(joinSearch.length > 0) {
-                res.send(joinSearch); 
-            } else {
-                res.send([{
-                    name: `No se Encontraron resultados realacionado a "${game}"`,
-                    img: "https://png.pngtree.com/png-vector/20210706/ourlarge/pngtree-no-result-search-icon-png-image_3563805.jpg"
-                }]);
-            }
-        } else {
+        //     if(joinSearch.length > 0) {
+        //         res.send(joinSearch); 
+        //     } else {
+        //         res.send([{
+        //             name: `No se Encontraron resultados realacionado a "${game}"`,
+        //             img: "https://png.pngtree.com/png-vector/20210706/ourlarge/pngtree-no-result-search-icon-png-image_3563805.jpg"
+        //         }]);
+        //     }
+        // } else {
 
             let apiGames = await getAllGames();
             if(!apiGames){
@@ -38,7 +38,7 @@ router.get('/', async(req,res, next)=> { //[ ] GET /videogames
             }
             res.send(apiGames)
 
-        }
+        // }
     } catch (error) {
         console.log(error)
         next(error)
