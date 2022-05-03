f270f663ddc3db2a7d4de3c9205b59f951e65fd6
cirorey20 <cirorey95@gmail.com>
2022-05-03 19:22:47 -0300

    added url firebase 1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const router = require('./src/routes/index.js');
 
 const { logErrors, errorHandler } = require('./middleware/errorHandler');
@@ -14,19 +14,19 @@ app.get('/', (req, res, next) => {
 })
 
 require('./src/strategies');
-// const whitelist = ['http://localhost:3000', 'https://myapiboyar.com'];
-// const options = {
-//   origin: (origin, callback) => {
-//     if (whitelist.includes(origin) || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('no permitido'))
-//     }
-//   }
-// }
-// app.use(cors(options));
+const whitelist = ['http://localhost:3000', 'https://videogamesciroapp.web.app'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'))
+    }
+  }
+}
+app.use(cors(options));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://videogamesciroapp.web.app/'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://videogamesciroapp.web.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
